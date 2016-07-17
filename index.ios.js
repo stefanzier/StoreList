beee203284b4ac4715dfeec9a7deed825ca1d88b
Stefan Zier <szier@icloud.com>
2016-07-17 12:04:06 -0700

    Add item to store list
    Saves data in firebase db

--- index.ios.js
+++ index.ios.js
@@ -3,23 +3,26 @@ import {
   AppRegistry,
   Text,
   View,
-  ListView
+  ListView,
+  AlertIOS
 } from 'react-native';
 
-import * as firebase from 'firebase';
+import firebase from 'firebase';
 import styles from './styles';
 import ActionButton from './components/action_button';
 import StatusBar from './components/status_bar';
 import ListItem from './components/list_item';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBeox5XiYhf2OvfnwBY-47QXDEHeslbX48',
-  authDomain: 'storelist-40269.firebaseapp.com',
-  databaseURL: 'https://storelist-40269.firebaseio.com',
-  storageBucket: 'storelist-40269.appspot.com',
+  apiKey: 'AIzaSyC-sZsoCG3yyxzHn9ZkNIpTrinI-oK5x-Q',
+  authDomain: 'storelist-bc8ac.firebaseapp.com',
+  databaseURL: 'https://storelist-bc8ac.firebaseio.com',
+  storageBucket: '',
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
+const rootRef = firebase.database().ref();
+const itemsRef = rootRef.child('items');
 
 class StoreList extends Component {
   constructor(props) {
@@ -28,12 +31,32 @@ class StoreList extends Component {
     this.state = {
       dataSource: new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2}),
     };
+
+    this.itemsRef = this.getRef().child('items');
+  }
+
+  getRef() {
+    return firebaseApp.database().ref();
   }
 
   componentDidMount() {
-    this.setState({
-      dataSource: this.state.dataSource.cloneWithRows([{ title: 'Pizza' }]),
-    })
+    this.listenForItems(this.itemsRef);
+  }
+
+  listenForItems(itemsRef) {
+    itemsRef.on('value', (snap) => {
+      var items = [];
+      snap.forEach((child) => {
+        items.push({
+          title: child.val().title,
+          _key: child.key
+        });
+      });
+
+      this.setState({
+        dataSource: this.state.dataSource.cloneWithRows(items)
+      });
+    });
   }
 
   _renderItem(item) {
@@ -46,9 +69,31 @@ class StoreList extends Component {
     return (
       <View style={styles.container}>
         <StatusBar title="Store List" />
-        <ListView dataSource={this.state.dataSource} renderRow={this._renderItem.bind(this)} style={styles.listview} />
-        <ActionButton title="Add" />
+
+        <ListView dataSource={this.state.dataSource} 
+                  renderRow={this._renderItem.bind(this)} 
+                  enableEmptySections={true}
+                  style={styles.listview} />
+
+        <ActionButton title="Add" onPress={this._addItem.bind(this)}/>
       </View>
+    );
+  }
+
+  _addItem() {
+    AlertIOS.prompt(
+      'Add new item',
+      null,
+      [
+        {
+          text: 'Add',
+          onPress: (text) => {
+            this.itemsRef.push({ title: text })
+          }
+        },
+      ],
+
+      'plain-text'
     );
   }
 }
